682249df13d464136f621a283af720baa13ff66e
Murdoch <swmurdoch@labs.neit.edu>
2018-03-14 11:43:23 -0400

    Uploaded the project

---
+++ Quiz Game/Assets/Scripts/MenuScreenController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScreenController : MonoBehaviour
+{
+    public GameObject playerName;
+    public Text playerNameText;
+    public GameObject startButton;
+
+    public void ButtonPress()
+    {
+        startButton.SetActive(false);
+        playerName.SetActive(true);
+    }
+
+    public void StartGame()
+	{
+        PlayerPrefs.SetString("playerName", playerNameText.text);
+        Debug.Log(PlayerPrefs.GetString("playerName"));
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
+    }
+}
